ab57b87572f88adad279c5fb17f8f8440c84e1fe
Ernestocracy <ogochukwujoseph33@gmail.com>
2023-10-18 03:38:51 -0700

    feat:pratices on html

---
+++ OOP.py
@@ -0,0 +1,107 @@
+# class student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# student1 = student("prosper", 20)
+# student2 = student("Joseph", 60)
+
+# print(student1.name)
+# print(student2.name, student2.age)
+
+# class phone:
+#     def __init__(self, brand, name, price):
+#         self.brand = brand
+#         self.name = name
+#         self.price = price
+
+
+# phone_1 = phone("Iphone", "14", 12000000)
+# phone_2 = phone("Samsug", "S9", 567654345)
+
+# print(phone_1.brand, phone_1.name, phone_1.price)
+# print(phone_2.brand, phone_2.name, phone_2.price)
+
+
+# class male:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+# male_1 = male("prosper" ,30)
+# male_2 = male("Joseph", 20)
+# print(male_1.name, male_1.age)
+
+
+
+
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+
+#     def profile(self):
+#         return f"your name is {self.name}, you are {self.age} year old"
+    
+# student1 = Student("Joseph", 20)
+# print(student1.profile())
+
+
+# class Car:
+#     def __init__(self, brand, year):
+#         self.brand = brand
+#         self.year = year
+
+# Car1 = Car("benz", 2007)
+# Car2 = Car("vovo", 2007)
+# print(Car1.brand, Car1.year)
+
+
+# class Employee:
+#     def __init__(self, name, age, job, salary):
+#         self.name = name
+#         self.age = age
+#         self.job = job
+#         self.salary = salary
+#     def bio(self):
+#         return f"your name is {self.name}, you earn {self.salary}"
+# Emp1 = Employee("Joseph", 34, "frontend", "340000")
+# print(Emp1.bio())
+
+
+
+# class Gadent:
+#     def __init__(self, type, brand, name, price):
+#         self.type = type
+#         self.brand = brand
+#         self.name = name
+#         self.price = price
+#     def get_name(self):
+#         return f"{self.get_name}"
+#     def price(self):
+#         return f"{self.price}"
+#     def full_info(self):
+#         return f"this is {self.type}, from {self.brand}, called {self.name}, worth {self.price}"
+# Gad1 = Gadent(" too small", "hp", "laptop", 2000)
+# print(Gad1.full_info())
+
+
+
+# class Calculator:
+#     def __init__(self, num1, num2):
+#         self.num1 = num1
+#         self.num2 = num2
+#     def add(self):
+#         return self.num1 + self.num2
+#     def sub(self):
+#         return self.num1 - self.num2
+#     def div(self):
+#         return self.num1 / self.num2
+#     def mult(self):
+#         return self.num1 * self.num2
+# num = Calculator(8,4)
+# print(num.add())
+# print(num.sub())
+# print(num.div())
+# print(num.mult())
+    
